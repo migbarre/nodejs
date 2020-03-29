0405395f8c4ec5aa74d3ac3f107ca3ad06f2e2f4
Miguel <miguelbarreto75@gmail.com>
2020-03-29 19:31:44 -0300

    Revert "Web server"
    This reverts commit 8726eb3955e989e009337144e52d8dd91c3b04de.

--- hbs/helpers.js
+++
@@ -1,9 +0,0 @@
-const hbs = require('hbs');
-
-hbs.registerHelper('getYear', () => {
-    return new Date().getFullYear();
-})
-
-hbs.registerHelper('toUppercase', (text) => {
-    return text.toUpperCase();
-})
